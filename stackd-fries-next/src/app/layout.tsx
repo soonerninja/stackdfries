57d624ee8f7dbf837a4c1b77334320589aeddd9d
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-29 04:59:02 +0000

    Install Vercel Speed Insights
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- stackd-fries-next/src/app/layout.tsx
+++ stackd-fries-next/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import PageTracker from '@/components/PageTracker';
 import "./globals.css";
 
@@ -134,6 +135,7 @@ export default function RootLayout({
         {children}
         <PageTracker />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
